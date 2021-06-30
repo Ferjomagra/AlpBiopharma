83b93fdc43004812ea3af6f26337b54cb82ee430
Ferjomagra <35279648+Ferjomagra@users.noreply.github.com>
2021-06-30 14:39:35 -0500

    Update Data.js

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -4,7 +4,7 @@ export const homeObjOne = {
     lightText: true,
     lightTextDesc: true,
     topLine: 'Nosotros',
-    headline: 'Comprometidos con lo que ofrecemos',
+    headline: 'Expertos con lo que ofrecemos',
     description: 'Somos un equipo con más de 10 años de experiencia en la comercialización de ingredientes y aditivos para el sector alimenticio, farmacéutico y nutrición, con los cuales, respondemos a la demanda usual de nuestros clientes.',
     description1: 'Trabajamos con los principales fabricantes de materias primas del exterior, líderes a nivel mundial, por ende, podemos brindarles precios competitivos, calidad y rápido stock.',
     buttonLabel: 'Contáctanos',
